25aff990def88051366335d8072d39edc215d18a
Praveen Mistry <praveensuthar108@gmail.com>
2019-06-01 10:07:20 +0530

    initial version

---
+++ readme.md
@@ -0,0 +1,9 @@
+## GraphQl APIs
+
+### To Install Dependency:
+	sudo npm i -g npm-check-updates
+	ncu -u
+
+### To Run:
+	cd src
+	npm start
---
+++ src/index.js
@@ -0,0 +1,34 @@
+// Import Server
+const fastify = require('./server.js')
+
+// Import external dependancies
+const gql = require('fastify-gql')
+
+// Import GraphQL Schema
+const schema = require('./schema')
+
+// Register Fastify GraphQL
+fastify.register(gql, {
+	schema,
+	graphiql: true
+})
+
+// Import Routes
+const routes = require('./routes/route')
+
+// Loop over each route
+routes.forEach((route, index) => {
+	fastify.route(route)
+})
+
+// Run the server!
+const start = async () => {
+	try {
+		await fastify.listen(3000, '0.0.0.0')
+		fastify.log.info(`server listening on ${fastify.server.address().port}`)
+	} catch (err) {
+		fastify.log.error(err)
+		process.exit(1)
+	}
+}
+start()
